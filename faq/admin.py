7ba6aee2175c7360aa4af2157aad0f5da211caa2
GabrielaRiestra222 <gabriela.riestra.lucas@gmail.com>
2026-05-19 11:43:33 +0200

    Constrain FAQ admin fields

--- faq/admin.py
+++ faq/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django import forms
 from .models import FAQCategory, FAQ
 
 
 class FAQInline(admin.TabularInline):
     model = FAQ
     extra = 1
+    formfield_overrides = {
+        FAQ._meta.get_field('question').__class__: {
+            'widget': forms.TextInput(attrs={'class': 'faq-admin-text'}),
+        },
+        FAQ._meta.get_field('answer').__class__: {
+            'widget': forms.Textarea(attrs={'class': 'faq-admin-textarea'}),
+        },
+    }
+
+    class Media:
+        css = {'all': ('faq/admin.css',)}
 
 
 @admin.register(FAQCategory)
@@ -18,3 +30,14 @@ class FAQAdmin(admin.ModelAdmin):
     list_display = ('question', 'category', 'is_published', 'order')
     list_filter = ('category', 'is_published')
     search_fields = ('question', 'answer')
+    formfield_overrides = {
+        FAQ._meta.get_field('question').__class__: {
+            'widget': forms.TextInput(attrs={'class': 'faq-admin-text'}),
+        },
+        FAQ._meta.get_field('answer').__class__: {
+            'widget': forms.Textarea(attrs={'class': 'faq-admin-textarea'}),
+        },
+    }
+
+    class Media:
+        css = {'all': ('faq/admin.css',)}
