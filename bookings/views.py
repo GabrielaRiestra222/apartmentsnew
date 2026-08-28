cacb1632b1378e8134d8ee6ad33fade826732658
GabrielaRiestra222 <gabriela.riestra.lucas@gmail.com>
2026-08-28 15:03:27 +0200

    Support calendar planner date ranges

--- bookings/views.py
+++ bookings/views.py
@@ -1,12 +1,19 @@
 from django.utils import timezone
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from common.permissions import OwnerScopedQuerysetMixin
 from .models import Booking
 from .serializers import BookingSerializer
+
+
+class BookingPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class BookingViewSet(OwnerScopedQuerysetMixin, viewsets.ModelViewSet):
@@ -19,12 +26,19 @@ class BookingViewSet(OwnerScopedQuerysetMixin, viewsets.ModelViewSet):
     serializer_class = BookingSerializer
     permission_classes = [IsAuthenticated]
     owner_lookup = 'apartment__owner'
+    pagination_class = BookingPagination
 
     def get_queryset(self):
         queryset = super().get_queryset()
         property_id = self.request.query_params.get('property') or self.request.query_params.get('apartment')
+        date_from = self.request.query_params.get('date_from') or self.request.query_params.get('start')
+        date_to = self.request.query_params.get('date_to') or self.request.query_params.get('end')
         if property_id:
             queryset = queryset.filter(apartment_id=property_id)
+        if date_from:
+            queryset = queryset.filter(check_out__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(check_in__lte=date_to)
         return queryset
 
     @action(detail=True, methods=['post'], url_path='return-deposit')
--- property_calendar/views.py
+++ property_calendar/views.py
@@ -1,17 +1,31 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from .models import CalendarBlock
 from .serializers import CalendarBlockSerializer
+
+
+class CalendarBlockPagination(PageNumberPagination):
+    page_size = 500
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class CalendarBlockViewSet(viewsets.ModelViewSet):
     queryset = CalendarBlock.objects.select_related('property', 'booking').order_by('start_date')
     serializer_class = CalendarBlockSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = CalendarBlockPagination
 
     def get_queryset(self):
         queryset = super().get_queryset()
         property_id = self.request.query_params.get('property') or self.request.query_params.get('apartment')
+        date_from = self.request.query_params.get('date_from') or self.request.query_params.get('start')
+        date_to = self.request.query_params.get('date_to') or self.request.query_params.get('end')
         if property_id:
             queryset = queryset.filter(property_id=property_id)
+        if date_from:
+            queryset = queryset.filter(end_date__gte=date_from)
+        if date_to:
+            queryset = queryset.filter(start_date__lte=date_to)
         return queryset
